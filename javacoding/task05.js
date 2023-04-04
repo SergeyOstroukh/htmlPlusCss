b1c92d29759ce006d39b82f5a255c21f9858dfb5
Sergey <hardmass3ffect@gmail.com>
2023-04-04 15:44:13 +0300

    add task05

---
+++ javacoding/task05.js
@@ -0,0 +1,10 @@
+// Make a function that will return a greeting statement that uses an input; 
+// your program should return,
+//  "Hello, <name> how are you doing today?"
+
+
+function greeting(name) {
+    return "Hello, " + name + " how are you doing today?";
+  }
+  
+  console.log(greeting("Sergey")); 
