a27e59cc59393d8a8d97e049c22de65d1138b917
Sergey <hardmass3ffect@gmail.com>
2023-04-05 12:24:21 +0300

    add new task

--- javacoding/task010.js
+++ javacoding/task010.js
@@ -1,3 +1,12 @@
-let num1 = -5;
-let res = num1 * (-1);
-console.log(res)
+function sum (numbers) {
+    let sum =0;
+    for(let i =0; i<numbers.length;i++){
+    if (typeof numbers[i] === 'number') {
+        sum+=numbers[i]   
+    }
+    
+    }
+return sum;
+}
+let numb = [1,5,6,7,"g"];
+  console.log(sum(numb))
